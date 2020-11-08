c5f5e79fd7db02c43839ac9e93f0861e0284d00a
ehayes2000 <eqhayes@protonmail.com>
2020-11-08 14:25:49 -0700

    refactor to use BoardType enum

--- haunted_tiles/emulator/unit_tests.py
+++ haunted_tiles/emulator/unit_tests.py
@@ -3,9 +3,10 @@ from haunted_tiles.emulator.board import Board
 from haunted_tiles.strategies import Strategy
 from haunted_tiles.emulator.game import Winner
 from haunted_tiles.strategies import Side
+from haunted_tiles.emulator.board import BoardType
 
 def get_basic_board():
-    basic_board = Board('basic')
+    basic_board = Board(BoardType.DEFAULT)
     basic_board.board = [[0 for _ in range(7)] for _ in range(7)]
     basic_board.board[0][0] = 3
     basic_board.board[6][0] = 3
