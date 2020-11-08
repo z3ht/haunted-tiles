cb67660c22a95101246e1a160e510cb9611611d9
Andrew <andrew@andrewdarling.xyz>
2020-11-08 12:14:25 -0700

    Add multiple maps skeleton

--- haunted_tiles/emulator/board.py
+++ haunted_tiles/emulator/board.py
@@ -1,20 +1,22 @@
-
 class Board:
     def __init__(self, board_type):
-        self._board_types = {'basic': [[3 for i in range(7)] for j in range(7)]}
-        self._starting_locations = {'basic': {'home': [(0, 0), (0, 3), (0, 6)], 'away': [(6, 0), (6, 3), (6, 6)]}}
-        self._board_sizes = {'basic': (7, 7)}
+        self._board_types = {
+            'default': [[3 for i in range(7)] for j in range(7)],
+            'waffle_town': None,
+            'jeff_bridges': None,
+            'i(n)': None,
+            'hourglass': None
+        }
 
         self.board = self._board_types[board_type]
-        self.home_start_locations = self._starting_locations[board_type]
-        self.away_start_locations = self._starting_locations[board_type]
-        self.board_size = self._board_sizes[board_type]
+        self.home_start_locations = [(0, 0), (0, 3), (0, 6)]
+        self.away_start_locations = [(6, 0), (6, 3), (6, 6)]
+        self.board_size = (7, 7)
         self.broken_tiles = set()
-
 
     def update_board(self, player_locations):
         """
-        :param occupied_tiles: iterable of positions that are occupied after players move
+        :param player_locations: all player locations as a tuple (x, y)
         """
         for y, x in player_locations:
             if self.board[y][x] > 0:
